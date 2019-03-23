953e15855fd7e9fb240532f751e509024eb9eeac
Kmiokande <brunnodesouzabezerra@gmail.com>
2019-03-23 14:51:45 -0300

    2612 - Os Atores Silva

---
+++ 2612.sql
@@ -0,0 +1,1 @@
+SELECT m.id, m.name FROM movies m JOIN genres g ON g.id = m.id_genres JOIN movies_actors ma ON m.id = ma.id_movies JOIN actors a ON a.id = ma.id_actors WHERE a.name LIKE 'Marcelo Silva' OR a.name LIKE 'Miguel Silva' AND g.description LIKE 'Action';
