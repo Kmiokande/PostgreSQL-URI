227942b945e45b756e560aa36df0b14ed238358a
Kmiokande <brunnodesouzabezerra@gmail.com>
2019-03-24 00:15:32 -0300

    2740 - Liga

---
+++ 2740.sql
@@ -0,0 +1,1 @@
+(SELECT CONCAT('Podium: ', team) AS name FROM league ORDER BY position LIMIT 3) UNION ALL (SELECT CONCAT('Demoted: ', team) AS name FROM league WHERE position > ANY (SELECT COUNT(*) - 2 FROM league));
